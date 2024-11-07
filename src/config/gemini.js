d4591447b33dee66f1ee9791e2d600a3f7b9eac3
tejastundalwar <tejastundalwar04@gmail.com>
2024-11-07 17:38:52 +0530

    first commit

---
+++ src/config/gemini.js
@@ -0,0 +1,47 @@
+
+
+// Polyfill for 'process' in the browser
+if (typeof process === 'undefined') {
+  window.process = {
+      env: {
+          GEMINI_API_KEY: 'AIzaSyA05Qp60LVFiAJDsfGYCpCRR7oMCjzVS_U' // Replace with your actual API key
+      }
+  };
+}
+
+
+import {
+    GoogleGenerativeAI,
+    HarmCategory,
+    HarmBlockThreshold,
+  } from "@google/generative-ai"
+  
+  const apiKey = process.env.GEMINI_API_KEY;
+  const genAI = new GoogleGenerativeAI(apiKey);
+  
+  const model = genAI.getGenerativeModel({
+    model: "gemini-1.5-flash",
+  });
+  
+  const generationConfig = {
+    temperature: 1,
+    topP: 0.95,
+    topK: 64,
+    maxOutputTokens: 8192,
+    responseMimeType: "text/plain",
+  };
+  
+  async function run(prompt) {
+    const chatSession = model.startChat({
+      generationConfig,
+      history: [
+      ],
+    });
+  
+    const result = await chatSession.sendMessage(prompt);
+    const response = result.response;
+    console.log(result.response.text());
+    return response.text();
+  }
+  
+  export default run;
